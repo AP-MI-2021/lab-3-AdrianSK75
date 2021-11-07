a48aa909c2170fcfdb2c3166d273ac3613605bc2
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2021-11-07 14:29:25 +0200

    Update assignment_3.py

--- assignment_3.py
+++ assignment_3.py
@@ -1,10 +1,12 @@
 def get_longest_div_k(lst, k):
+        """
+        determina cea mai lunga subsecv care este div cu k
+        """
         max_div_k = div_k = start = end = 0
         for i in range(len(lst)):
             if lst[i] % k == 0:
                 div_k+=1
                 if max_div_k < div_k:
-                    print(i)
                     max_div_k = div_k
                     start = i - div_k + 1
                     end = i
@@ -15,21 +17,58 @@ def get_longest_div_k(lst, k):
             print(lst[start])
             start += 1
 
-def read_array():
-    problem = input("Choose the problem: ")
-    length = int(input("Read the length of array:"))
-    lst = []
-    print("Read the array elements:")
+def is_palindrome(n):
+    copie = n
+    invers = 0
+    while copie != 0:
+        cifra = copie % 10
+        invers = invers * 10 + cifra
+        copie = copie//10
+
+    if invers == n:
+        return 1
+    return 0
+
+def get_longest_all_palindromes(lst):
+    """
+    determina cea mai lunga subsecv de palindroame
+    """
+    l = len(lst)
+    result = []
+    for i in range(l):
+        for j in range(i, l):
+            k = is_palindrome(lst[i])
+            all_palindromes = True
+            for num in lst[i:j + 1]:
+                if is_palindrome(num) != k:
+                    all_palindromes = False
+                    break
+            if all_palindromes:
+                if j - i + 1 > len(result):
+                    result = lst[i:j + 1]
+    return result
+
+def read_array(lst, length):
+    print("Introduceti numerele sirului: ")
     for i in range(length):
         lst.append(int(input()))
-    #print(lst)
-    if problem == "6":
-       k = int(input("K element: "))
-       get_longest_div_k(lst, k)
 
-
+def read_statements():
+    problem = input("Introduceti numarul problemei ")
+    length = int(input("Introduceti lungimea sirului: "))
+    lst = []
+    if problem == "1":
+        read_array(lst, length)
+        print(get_longest_all_palindromes(lst))
+    elif problem == "2":
+        k = int(input("Elementul K: "))
+        read_array(lst, length)
+        get_longest_div_k(lst, k)
 
 def main():
-    read_array()
+    print("1. Sa se determine cea mai lunga subsecventa de palindroame")
+    print("2. Sa se determine cea mai lunga subseceventa divizibila cu k")
+    read_statements()
+
 if __name__ == '__main__':
     main()
